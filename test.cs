99ad0aeee2d64007354db32925be3964d38db171
ItoTaichi <taityowa.ajidesu111@softbank.ne.jp>
2021-09-24 03:29:16 +0900

    first game project

---
+++ test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Hello, Worldをコンソールウィンドウに表示する
+        Debug.Log("Hellow, World");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
